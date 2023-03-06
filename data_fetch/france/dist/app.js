b4800bcde9b388a35222f333af9b33217b82bb99
regis portalez <1440561+JeWaVe@users.noreply.github.com>
2023-03-06 17:06:36 +0100

    remove generated files

--- data_fetch/france/dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./api_pappers/api");
-const api_1 = require("./api_pappers/api");
-var toto = new api_1.FicheEntrepriseApi();
-//# sourceMappingURL=app.js.map
